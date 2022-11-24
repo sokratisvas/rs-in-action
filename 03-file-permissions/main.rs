d06cfd46eb4101fffd4fa0cb15fc5239ea1e2509
Sokratis Vasiliou <sokpanvas@gmail.com>
2022-11-25 00:15:34 +0200

    Fully working

--- 03-file-permissions/main.rs
+++ 03-file-permissions/main.rs
@@ -3,8 +3,32 @@ use std::os::linux::fs::MetadataExt;
 const S_IFDIR: u32 = 0o040000;
 const S_MASK: u32 = 0o170000;
 
-fn S_IFMT(st_mode: u32) -> u32{
+fn S_IFMT(st_mode: u32) -> u32 {
     st_mode & S_MASK 
+}
+
+fn beautify_rwx(perm: &str) -> String {
+    let full_word = |permission: char| -> &str {
+        match permission {
+            'r' => "read",
+            'w' => "write",
+            'x' => "execute",
+            '-' => "not_set",
+            _ => "that's bad"
+        }
+    };
+    //let format: Vec<&str> = perm.chars().map(|x| full_word(x)).collect::<Vec<&str>>().join(", ");
+    let format = perm.chars().map(|x| full_word(x))
+        .fold("".to_string(), |mut res, permission| {
+            if permission != "not_set" {
+                res += permission;
+                res += ", ";
+            }
+            res
+        })
+        .to_string();
+    let trimmed = format.trim_end_matches(", ");
+    trimmed.to_string()
 }
 
 fn filemode(st_mode: u32) {
@@ -19,7 +43,6 @@ fn filemode(st_mode: u32) {
     let mut digit: usize = 1;
     let mut mode = st_mode;
     while digit <= 3 {
-        println!("{}", mode);    
         match mode % 8 {
             7 => unix_name[4 - digit] = "rwx",
             6 => unix_name[4 - digit] = "rw-",
@@ -31,14 +54,21 @@ fn filemode(st_mode: u32) {
         mode /= 8;
         digit += 1;
     }
-    println!("{:?}", unix_name);    
+    for owner in 1..=3 {
+        let curr = match owner {
+            1 => "User",
+            2 => "Groups",
+            3 => "Other",
+            _ => "no way"
+        };
+        let beaut = beautify_rwx(unix_name[owner]);
+        println!("{}: {}", curr, beaut);
+    }
+
 }
 
 fn main() -> std::io::Result<()> {
     let meta = std::fs::metadata("file")?;
-    //println!("{:o}", meta.st_mode() & 07777);
-    println!("{:o}", meta.st_mode());
-    //println!("{}", meta.st_mode());
     filemode(meta.st_mode());
     Ok(())
 }
